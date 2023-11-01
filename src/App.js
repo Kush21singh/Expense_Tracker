3d6a42e249545a02dadd2042f81787092d549b37
Kush21singh <ankush76728@gmail.com>
2023-11-01 19:44:47 +0530

    logout_user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from "react";
+
 import './App.css';
-import Login from './components/Login';
+import Home from './components/Pages/Home';
+import Login from './components/Pages/Login';
+import SignUp from './components/Pages/Signup';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import ForgotPassword from './components/Pages/ForgotPassword'
+import React, {  useEffect } from 'react';
+import { ExpenseAction } from './store/ExpenseSlice';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { authActions } from './store/AuthSlice';
+import NewExpense from './components/HomeComponents/NewExpense';
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+  const dispatch = useDispatch()
+  const { changeData } = ExpenseAction
+  const { activatePremium } = authActions
+  // const history=useNavigate()
+  const GetDataFromDataBase = async (user) => {
+    try {
+      const res = await axios.get(`https://expense-tracker-74dbe-default-rtdb.firebaseio.com/${user}.json`)
+      if (res.data.expense) {
+        dispatch(changeData(res.data))
+      }
+        dispatch(activatePremium(res.data.premium))
+    
+    } catch (error) {
+    }
+  }
+
+  useEffect(() => {
+    // !isLoggedIn&&history('/signup')
+    const useremail = localStorage.getItem('email')
+   if(useremail){
+    const user = useremail.replace('@', "").replace('.', "")
+    GetDataFromDataBase(user)
+  }
+  
+  }, [dispatch,isLoggedIn])
+  const showForm=useSelector(state=>state.expenseForm.showForm)
+  return (<div style={{backgroundColor:''}}>
+    {showForm&&ReactDOM.createPortal(<NewExpense/>,document.getElementById('add-expense'))}
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<Login />} />
+      <Route path='/forgotpassword' element={<ForgotPassword />} />
+
+    </Routes>
     </div>
   );
 }
