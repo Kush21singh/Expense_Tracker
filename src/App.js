a4523e24d5e891614de259671e25352b6da238ae
Kush21singh <ankush76728@gmail.com>
2023-11-01 21:00:02 +0530

    Forgot_Password

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
-import './App.css';
-import Login from './components/Login';
 
+import './App.css';
+import Home from './components/Pages/Home';
+import Login from './components/Pages/Login';
+import SignUp from './components/Pages/Signup';
+import { Route,Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ForgotPassword from './components/Pages/ForgotPassword'
 function App() {
+  const isLoggedIn=useSelector(state=>state.auth.isLoggedIn)
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Routes>
+      
+      <Route path='/' element={isLoggedIn&&<Home/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/signin' element={<Login/>}/>
+      <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+    
+    </Routes>
   );
 }
 
